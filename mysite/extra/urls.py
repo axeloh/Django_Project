d32a7e8bbfd728b235008ca7a176e67b77e3fb14
axeloh <axel.harstad@gmail.com>
2018-04-30 11:55:44 +0200

    endret static-strukturen

--- mysite/extra/urls.py
+++ mysite/extra/urls.py
@@ -3,6 +3,4 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('popup.css', views.popup_css, name='popup_css'),
-    path('popup.js', views.popup_js, name='popup_js')
 ]
